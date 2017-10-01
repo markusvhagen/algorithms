c3c47593aa533b14c40a3a4a3af67c9f6b837584
markusvhagen <markusvhagen@gmail.com>
2017-10-01 22:30:06 +0200

    binary search in python and javascript

---
+++ python/searching/binarySearch.py
@@ -0,0 +1,26 @@
+import math
+
+testArray = [1, 3, 4, 5, 9, 11, 18, 22, 31, 42, 69, 100, 101, 102, 450, 999];
+
+def binarySearch(array, searchFor):
+    minIndex = 0
+    maxIndex = len(array) - 1
+    midIndex = math.floor((minIndex + maxIndex)/2)
+
+    while maxIndex > minIndex:
+        midIndex = math.floor((minIndex + maxIndex)/2)
+
+        if searchFor < array[midIndex]:
+            print("lower")
+            maxIndex = midIndex - 1
+
+        elif searchFor > array[midIndex]:
+            print("higher")
+            minIndex = midIndex + 1
+
+        elif searchFor == array[midIndex]:
+            print("Found element at index " + str(midIndex))
+            break
+
+nInput = input("Search for: ")
+binarySearch(testArray, int(nInput))
